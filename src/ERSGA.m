462b6c16f0e860d651a4c8abfb5c13b892083802
Martin Higgs <martinhiggs85@googlemail.com>
2015-11-24 11:47:04 +0000

    Commented structure of algorithm

---
+++ src/ERSGA.m
@@ -0,0 +1,11 @@
+function [ change ] = ERSGA( L_done, C_done, O, l, base )
+%ERSGA EZ Random Solution Generation Algorithm
+%   Detailed explanation goes here
+
+% TODO: Calculate parameters of uniquely solvable APs (C_done)
+% TODO: Calculate locations of new locations and loop (L_done)
+
+%X = linsolve(A,B) where AX = B
+
+end
+
---
+++ src/main.m
@@ -0,0 +1,14 @@
+% Main Matlab script file
+% Author: mh881@york.ac.uk
+
+% TODO: Load data
+
+% TODO: Identify set of most useful APs
+
+% TODO: Identify set of most useful locations
+
+% TODO: ERSGA
+
+% TODO: Generate RSS Map
+
+% And beyond...
