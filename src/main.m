d2e01f451baf87e5339a6b23ae7f93099c244cd7
Martin Higgs <martinhiggs85@googlemail.com>
2015-11-26 17:10:01 +0000

    Fix the fudge

--- src/main.m
+++ src/main.m
@@ -1,7 +1,6 @@
 % Main Matlab script file
 % Author: mh881@york.ac.uk
 
-<<<<<<< HEAD
 % Load raw data from CSV
 srcData = csvread('trainingData.csv', 1);
 
@@ -11,9 +10,6 @@ for j = find(srcData(:,1)<0)
     O = [O; srcData(j,1), srcData(j,521), srcData(j,522)];
 end
 APSolve(O)
-=======
-% TODO: Load data
->>>>>>> 462b6c16f0e860d651a4c8abfb5c13b892083802
 
 % TODO: Identify set of most useful APs
 
