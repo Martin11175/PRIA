6daa043f4b896b481071ce984019b1ed1d917c3e
Martin Higgs <martinhiggs85@googlemail.com>
2016-02-22 08:50:45 +0000

    Intermediary commit: considering linsolve for APSolve instead of simulated annealing, SimpleRGEA requires more efficient implementation (currently still evaluates all zeros?)

--- src/SimpleRGEA.m
+++ src/SimpleRGEA.m
@@ -4,11 +4,21 @@ function [ G ] = SimpleRGEA( J, K )
 %   K [in] - Vector of device IDs relating rows to device
 %   G [out] - Pairs of device IDs (1) to estimated gain (2)
 
-D = sort(unique(K)); % List of device IDs
-J(J == 100) = -100; % Replace positive invisibility markers to prevent skew
+prox_threshold = 3;
+min_AP_overlap = 2;
+num_strongest_APs = 4;
+min_AP_strength = -70;
 
-% TODO: Isolate strongest RSSI measurements per location
-[~, order] = sort(J(1,:));
+D = sort(unique(K)); % List of device IDs
+J(J == 100) = -100; % Replace positive invisibility markers
+
+% Isolate strongest RSSI measurements per location
+for i = 1:size(J,1)
+    [~, order] = sort(J(i,:));
+    J(i, order(1:(size(J,2) - num_strongest_APs))) = 0;
+end
+J(J < min_AP_strength) = 0;
+J = sparse(J); % TODO: Is sparse faster?
 
 % Calculate relative gain between pairs of devices
 deltaG = zeros(size(D,1));
@@ -16,26 +26,23 @@ sigma_deltaG = zeros(size(D,1)); % Uncertainty (estimated standard deviation)
 
 for i = nchoosek(1:size(D,1), 2)'
     % Test pairs of measurements for proximity by similarity
-    avg_diff = zeros(sum(K == D(i(1))), sum(K == D(i(2))));
+    diff = zeros(sum(K == D(i(1))), sum(K == D(i(2))), size(J,2));
+    m = J(K == D(i(1)), :);
+    n = J(K == D(i(2)), :);
     
-    count_m = 1;
-    for m = J(K == D(i(1)), :)'
-        count_n = 1;
-        for n = J(K == D(i(2)), :)'
-            % Only compare APs visible at at least one location
-            vis = (m > -100) | (n > -100);
-            if(sum(vis) > min_AP_overlap && mean(abs(m(vis) - n(vis))) < prox_threshold)
-                avg_diff(count_m, count_n) = mean(m(vis) - n(vis));
-            end
-            count_n = count_n + 1;
-        end
-        count_m = count_m + 1;
+    for k = 1:520
+        diff(:,:,k) = bsxfun(@minus, m(:,k), n(:,k)');
     end
     
+    tic
     % Compare all proximate locations
-    prox = abs(avg_diff) < prox_threshold & (avg_diff ~= 0);
+    num_compared = sum(diff ~= 0, 3);
+    sum_abs_diff = sum(abs(diff),3);
+    avg_abs_diff = (sum_abs_diff ./ num_compared);
+    prox = (avg_abs_diff < prox_threshold) & (num_compared > min_AP_overlap);
     
     if sum(sum(prox)) > 10
+        avg_diff = (sum(diff,3) ./ num_compared);
         deltaG(i(1), i(2)) = mean(avg_diff(prox));
         sigma_deltaG(i(1), i(2)) = (1 / sum(sum(prox))) * sqrt(sum((avg_diff(prox) - deltaG(i(1), i(2))).^2));
     end
