8629048af61543fce5f68f6568c05de1d21dae2c
Martin Higgs <martinhiggs85@googlemail.com>
2016-04-03 16:45:33 +0100

    Removed offending curly brace. Restricted initial tests of main.

--- src/model_tests.sh
+++ src/model_tests.sh
@@ -7,14 +7,14 @@
 rgea_types=( "'ground'" "'rgea'" "'simple'" "'none'" )
 ap_types=( "'all'" "'overlap'" "'max'" "'none'" )
 loc_types=( "'all'" "'overlap'" "'max'" "'none'" )
-ips_types=( "'all'" "'max'" )
+ips_types=( "'all'" ) #( "'all'" "'max'" )
 
 # Model generation parameters
 thresholds="[-100 -95 -90 -85 -80 -75 -70 -65 -60 -55 -50]"
 buildings="[0 1 2 3]"
 floors="[0 1 2 3 4]"
-bounds=( 0 5 10 15 20 25 30 40 50 75 100 )
-chances=( 100 75 50 30 25 20 15 10 5 )
+bounds=( 20 ) #( 0 5 10 15 20 25 30 40 50 75 100 )
+chances=( 100 ) #( 100 75 50 30 25 20 15 10 5 )
 
 # Do every combination of parameters and parts
 for rgea_type in ${rgea_types[@]}
@@ -33,7 +33,7 @@ do
                         # Run tests
                         nohup matlab -nosplash -nojvm -nodisplay -r "\
                         rgea_type = $rgea_type;\
-                        ap_type = $ap_type};\
+                        ap_type = $ap_type;\
                         loc_type = $loc_type;\
                         ips_type = $ips_type;\
                         bounds = $bound;\
